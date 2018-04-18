46007c9645ef1f74582581d5114a77283de09def
Chen <s3629986@student.rmit.edu.au>
2018-04-18 10:13:21 +1000

    login page

---
+++ PP1/js/login.js
@@ -0,0 +1,23 @@
+function login() {  
+  
+    var username = document.getElementById("username");  
+    var pass = document.getElementById("password");  
+  
+    if (username.value == "") {  
+  
+        alert("Please enter a username");  
+  
+    } else if (pass.value  == "") {  
+  
+        alert("Please enter a password");  
+  
+    } else if(username.value == "admin" && pass.value == "123456"){  
+  
+        window.location.href="welcome.html";  
+  
+    } else {  
+  
+        alert("Please enter the correct username and password!")  
+  
+    }  
+}  
